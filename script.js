7f0db3d341230d60b0ffa9047204de0a0e19673d
lkodovski <l.kodovski@hotmail.com>
2023-10-19 14:16:30 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,111 @@
+// console.log("Jonas");
+// console.log(23);
+
+// let firstName = "Matilda";
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// let jonas_matilda = "JM";
+// let $function = 27;
+
+// let person = "jonas";
+// let PI = 3.1415;
+
+// let myFirstJob = "programmer";
+// let myCuurentJob = "teacher";
+
+// let job1 = "Coder";
+// let job2 = "teacher";
+
+// console.log(myFirstJob);
+
+// let javaScriptIsFun = true;
+// console.log(javaScriptIsFun);
+
+// console.log(typeof javaScriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+
+// javaScriptIsFun = "YES!!";
+// console.log(typeof javaScriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = "1991";
+// console.log(typeof year);
+
+// // let, var, const
+
+// let age = 30;
+// age = 31;
+// console.log(age);
+
+// const birthYear = 1991;
+// console.log(birthYear);
+
+// var job = "teacher";
+// job = "Coder";
+
+// console.log(job);
+
+// Basic Operators
+
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2, ageSarah / 10, 2 ** 3);
+
+// const firstName = "Jonas";
+// const lastName = "Smetman";
+// console.log(firstName + " " + lastName);
+
+// let x = 10 + 5;
+// x += 10;
+// x *= 4;
+// x++;
+// x++;
+// x--;
+// console.log(x);
+
+// console.log(ageJonas > ageSarah);
+// console.log(ageSarah >= 18);
+
+// const isFullAge = ageSarah <= 18;
+
+//String and Template Literals
+
+// const firstName = "Jonas";
+// const job = "teacher";
+// const birthYear = 1991;
+// const year = 2037;
+
+// const jonas =
+//   "I'm " + firstName + ", a " + (year - birthYear) + " year old " + job + "!";
+// console.log(jonas);
+
+// const jonasNew = `I'm ${firstName}, a ${year - birthYear} year ld ${job}!`;
+// console.log(jonasNew);
+
+// Ja sum $Marija od $III6 klas i momentalno sum na cas po $Web programiranje
+// const firstName = "Marija";
+// const klas = "III6";
+// const predmet = "Web programiranje";
+// const year = 2037;
+// const podatoci = `Jas sum ${firstName}, od ${klas} klas i momentalno sum na cas po ${predmet}!`;
+// console.log(podatoci);
+
+// // if/else statements
+
+// const age = 18;
+
+// if (age >= 18) {
+//   console.log("Sarah can start driving license");
+// } else {
+//   const yearsLeft = 18 - age;
+//   console.log(`Sarah is to young. Wait for another ${yearsLeft} years`);
+// }
